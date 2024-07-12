69dc7eacaf8f10a51f09a8ac67cebdaad0b21f09
sliftist <sliftist@gmail.com>
2024-07-12 19:11:04 -0400

    Use the native API, so we get cross domain support.

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 let assigneeCache = new Map();
-async function getAssignee(ticket) {
+async function getAssignee(ticket, href) {
     let result = assigneeCache.get(ticket);
-    if(result === undefined) {
+    if (result === undefined) {
         let url = location.origin + "/rest/api/3/issue/" + ticket + "?fields=assignee";
         result = fetch(url).then(json => json.json()).then(x => x.fields.assignee.displayName);
+
+        let rawResult = fetch(location.origin + "/gateway/api/object-resolver/resolve/batch", {
+            "headers": {
+              "accept": "application/json",
+              "accept-language": "en-US,en;q=0.9",
+              "cache-control": "no-cache",
+              "content-type": "application/json",
+              "origin-timezone": "America/Toronto",
+              "pragma": "no-cache",
+              "priority": "u=1, i",
+              "x-product": "CONFLUENCE"
+            },
+            body: JSON.stringify([{ resourceUrl: href }]),
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include",
+        });
+
+        result = rawResult.then(json => json.json()).then(x => x[0].body.data["atlassian:assignedTo"]?.name || "Unassigned");
+
         assigneeCache.set(ticket, result);
     }
     return result;
@@ -12,12 +32,12 @@ async function getAssignee(ticket) {
 function apply() {
     let time = Date.now();
     let links = Array.from(document.querySelectorAll("[data-testid=inline-card-resolved-view]"));
-    for(let link of links) {
-        if(link.updated) continue;
+    for (let link of links) {
+        if (link.updated) continue;
         console.log(`Updating jira link`);
         link.updated = true;
         let ticket = link.href.split("/").slice(-1)[0];
-        getAssignee(ticket).then((assignee) => {
+        getAssignee(ticket, link.href).then((assignee) => {
             let example = link.children[link.children.length - 1];
             let class1 = example.className;
             let child = example.children[example.children.length - 1];
@@ -39,7 +59,7 @@ function apply() {
         });
     }
     time = Date.now() - time;
-    if(time > 20) {
+    if (time > 20) {
         console.warn(`Updating jira links took a long time (${time}ms)`);
     }
 }
